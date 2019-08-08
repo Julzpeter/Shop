83c6251f3ad6a4d47077409720bf4b2bf591c5e5
Juliet <chepngetichjuliet@gmail.com>
2019-08-08 14:27:20 +0300

    adds class meta

--- shop/models.py
+++ shop/models.py
@@ -25,7 +25,13 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
+    class Meta:
+        ordering=('name',)
+        index_together = (('id','slug'),)
+
+    def __str__(self):
+        return self.name
 
 
 
