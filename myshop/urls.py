c30a57b23c8a17279196eb8178e4d118efc851cd
Juliet <chepngetichjuliet@gmail.com>
2019-08-13 15:48:48 +0300

    adds media

--- myshop/urls.py
+++ myshop/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^',include('shop.urls', namespace='shop'))
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
